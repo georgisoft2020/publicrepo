95f9d0cb093ed4d01a5479e62f5c5be1aad44c07
Georgi Nikolov <74551563+georgisoft2020@users.noreply.github.com>
2022-11-16 16:01:51 +0200

    Create missing-values.php

---
+++ interview-task/missing-values.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Given an array arr[] of size N-1 with integers in the range of [1, N], the task is to find the missing number from the first N integers.
+ *
+ * Note: There are no duplicates in the list. The values are not sorted.
+ *
+ * Examples:
+ *
+ * Input: $arr = [1, 2, 4, 6, 3, 7, 8], N = 8
+ * Output: 5
+ * Explanation: The missing numbers between 1 to 8 are: 5
+ *
+ * Input: $arr = [1, 3, 5], N = 5
+ * Output: 2 4
+ * Explanation: The missing numbers between 1 to 5 are: 2 4
+ **/
+
+function printMissingNumbersFrom1to100(array $inputArr, int $N)
+{
+}
+
+printMissingNumbersFrom1to100([1, 4, 2, 7], 10);
+
+/**
+ * Should output: 3 5 6 8 9 10
+ */
